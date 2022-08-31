2666d651a7899714596d8f583fda066111ece2f1
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-08-31 18:35:20 -0300

    feat: variavel de ambiente

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const env = process.env.ENV;
+
 app.get('/', (_req, res) => {
-  res.status(200).send('Está vivo!!!')
+  res.status(200).send(
+    `Está vivo!!!
+    Ambiente de ${env}`
+  )
 });
 
 const port = process.env.PORT || 3000;
